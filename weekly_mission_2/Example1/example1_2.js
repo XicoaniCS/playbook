3593eac85dc086743113e61bba54aacf4b8923e2
XicoaniCS <m.casasolaromero.66@gmail.com>
2022-04-20 22:07:58 -0500

    Adding example 2 with a small code error

---
+++ weekly_mission_2/Example1/example1_2.js
@@ -0,0 +1,13 @@
+console.log("2) Creación de un objeto con propiedades")
+const explorer = {
+    name: "Xicoani",
+    age: 20,
+    mission: "Node JS",
+    stack: ["JS", "node", "react", "vue"],
+    blog: { url: "github.com/XicoaniCS/blog" } //Esto es un objeto 
+}
+console.log(explorer)
+
+console.log("Accediendo a las propiedades del objeto")
+console.log(explorer.age)
+console.log(explorer["agess"])
