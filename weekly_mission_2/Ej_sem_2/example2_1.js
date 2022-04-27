bd60e013eb7f95de814915dea409f03e53704f4b
XicoaniCS <m.casasolaromero.66@gmail.com>
2022-04-26 22:10:57 -0500

    Adding examples of how add a metod to the class

--- weekly_mission_2/Ej_sem_2/example2_1.js
+++ weekly_mission_2/Ej_sem_2/example2_1.js
@@ -1,3 +1,11 @@
+/* 
+1. Como crear un clase 
+2. Como instanciar un objeto de una clase
+3. Instanciar un objeto de una misma clase
+4. Como agregar el constructor para guardar atributos de una clase
+5. Crear métodos
+*/
+
 console.log("POO")
 
 //Objeto de JS
@@ -16,7 +24,20 @@ class Ajolonauta {
         this.age = age
         this.color = color
     }
+
+    sayHello() {
+        return `${this.name} is saying Hello!`
+    }
 }
+
+class Pokemon {
+    constructor(name) {
+        this.name = name
+    }
+}
+
+const pikachu = new Pokemon("Pikachu")
+console.log(pikachu)
 
 //Objeto de JS
 const woopa_obj_js = {}
@@ -24,6 +45,8 @@ console.log(woopa_obj_js)
 
 const woopa = new Ajolonauta("Woopa", "Node JS", 18, "Pink") //Instanciar un objeto
 console.log(woopa)
+console.log(woopa.sayHello())
 
 const wooper = new Ajolonauta("Wooper", "Java", 9, "Purple")
 console.log(wooper)
+console.log(wooper.sayHello())
